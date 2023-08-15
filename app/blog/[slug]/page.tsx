842e0fddc8adda621aaeecb9cd5992a4e7534d74
AceDev <bjmczhang@gmail.com>
2023-08-15 12:03:59 +1000

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,39 +1,16 @@
 import { getPostBySlug } from "@/lib";
-import { GetStaticProps, GetStaticPaths } from "next";
 
-interface PageParams {
-  slug: string;
-}
-
-interface PageMeta {
-  title: string;
-  // Add other meta properties here if needed
-}
-
-interface PageContent {
-  meta: PageMeta;
-  content: string;
-}
-
-const getPageContent = async (slug: string): Promise<PageContent> => {
+const getPageContent = async (slug: any) => {
   const { meta, content } = await getPostBySlug(slug);
   return { meta, content };
 };
 
-export const generateMetadata: GetStaticProps = async ({ params }) => {
-  if (!params) {
-    return { props: {} }; // Handle the case when params is undefined
-  }
-
+export async function generateMetadata({ params: any }) {
   const { meta } = await getPageContent(params.slug);
-  return { props: { title: meta.title } };
-};
+  return { title: meta.title };
+}
 
-const Page = async ({ params }: { params: PageParams }) => {
-  if (!params) {
-    return <div>Loading...</div>; // Handle the case when params is undefined
-  }
-
+const Page = async ({ params: any }) => {
   const { content } = await getPageContent(params.slug);
 
   return (
